f7c7bd1fe539789eecac5b549c1e11d8067f0577
marcosricardo <mricardoalves99@gmail.com>
2019-10-27 00:47:13 -0300

    Create component photo

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:"app-photo",
+    templateUrl:"photo.component.html"
+})
+
+export class PhotoComponent {
+    title = 'photolibrary';
+    url = "https://149366112.v2.pressablecdn.com/wp-content/uploads/2019/09/shutterstock_547175674.jpg"
+}
