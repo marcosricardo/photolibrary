4076604252bf6e36f944d9fd4857f49261a3b324
marcosricardo <mricardoalves99@gmail.com>
2019-10-27 11:45:54 -0300

    Page 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 import { PhotosModule } from './photos/photos.module';
+import { ErrorsModule } from './errors/errors.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { PhotosModule } from './photos/photos.module';
   imports: [
     BrowserModule,
     PhotosModule, 
+    ErrorsModule,
     AppRoutingModule
   ],
   providers: [],
