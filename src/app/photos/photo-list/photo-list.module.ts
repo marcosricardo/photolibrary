0044f86084d7cad2ac0cbcf23ce74cdbac99cca7
marcosricardo <mricardoalves99@gmail.com>
2019-11-03 11:59:58 -0300

    Directive Dark On Hover

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -3,12 +3,11 @@ import { PhotoListComponent } from './photo-list.component';
 import { PhotosComponent } from './photos/photos.component';
 import { LoadButtonComponent } from './load-button/load-button.component';
 import { SearchComponent } from './search/search.component';
-
 import { FilterByDescription } from './filter-by-description.pipe';
-
 import { CommonModule } from '@angular/common';
 import { PhotoModule } from '../photo/photo.module';
-import { CardModule } from '../../shared/components/card/card.module'
+import { CardModule } from '../../shared/components/card/card.module';
+import { DarkenOnHoverModule } from '../../shared/directives/darken-on-hover/darken-on-hover.module';
 
 
 @NgModule({
@@ -19,6 +18,6 @@ import { CardModule } from '../../shared/components/card/card.module'
         SearchComponent,
         FilterByDescription
     ],
-    imports: [ CommonModule, PhotoModule, CardModule ]
+    imports: [ CommonModule, PhotoModule, CardModule, DarkenOnHoverModule  ]
 })
 export class PhotoListModule {}
