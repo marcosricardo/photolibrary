cb874a14b6ed846206dd1d611ee15a74a1f5c433
marcosricardo <mricardoalves99@gmail.com>
2019-11-03 09:01:32 -0300

    Sub Modules

---
+++ src/app/photos/photo/photo.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { PhotoComponent } from './photo.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+    declarations: [PhotoComponent],
+    imports: [ CommonModule ],
+    exports: [ PhotoComponent ]
+})
+export class PhotoModule { }
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -2,20 +2,22 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
+import { PhotoModule } from './photo/photo.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
 import { PhotoComponent } from './photo/photo.component';
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { FilterByDescription } from './photo-list/filter-by-description.pipe';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
 
 
 @NgModule({
-    declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent, FilterByDescription, LoadButtonComponent], 
+    declarations: [ ], 
     imports:[
+        PhotoModule,
+        PhotoFormModule,
+        PhotoListModule,
         HttpClientModule,
         CommonModule
-    ]
+    ],
+    exports: [  ]
 })
 
 export class PhotosModule {}
